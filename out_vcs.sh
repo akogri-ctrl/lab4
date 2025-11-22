f66b59f08842df57f6baca7977e1487582e638e1
akogri-ctrl <akogri@asu.edu>
2025-11-22 21:49:10 +0000

    out

---
+++ out_vcs.sh
@@ -0,0 +1,56 @@
+=========================================
+GCN Lab4 - VCS Simulation
+=========================================
+Compiling RTL files...
+                         Chronologic VCS (TM)
+      Version V-2023.12-SP1-1_Full64 -- Sat Nov 22 21:47:01 2025
+
+                    Copyright (c) 1991 - 2024 Synopsys, Inc.
+   This software and the associated documentation are proprietary to Synopsys,
+ Inc. This software may only be used in accordance with the terms and conditions
+ of a written license agreement with Synopsys, Inc. All other use, reproduction,
+   or distribution of this software is strictly prohibited.  Licensed Products
+     communicate with Synopsys servers for the purpose of providing software
+    updates, detecting software piracy and verifying that customers are using
+    Licensed Products in conformity with the applicable License Key for such
+  Licensed Products. Synopsys will use information gathered in connection with
+    this process to deliver software updates and pursue software pirates and
+                                   infringers.
+
+ Inclusivity & Diversity - Visit SolvNetPlus to read the "Synopsys Statement on
+            Inclusivity and Diversity" (Refer to article 000036315 at
+                        https://solvnetplus.synopsys.com)
+
+Parsing design file 'Starter RTL Code/DFF.sv'
+Parsing design file 'Starter RTL Code/DotProduct.sv'
+Parsing design file 'Starter RTL Code/Argmax.sv'
+Parsing design file 'Starter RTL Code/Scratch_Pad.sv'
+Parsing design file 'Starter RTL Code/Matrix_FM_WM_Memory.sv'
+Parsing design file 'Starter RTL Code/Matrix_FM_WM_ADJ_Memory.sv'
+Parsing design file 'Starter RTL Code/Transformation_FSM.sv'
+Parsing design file 'Starter RTL Code/GCN.sv'
+
+Error-[IND] Identifier not declared
+Starter RTL Code/GCN.sv, 249
+  Identifier 'class_state' has not been declared yet. If this error is not 
+  expected, please check if you have set `default_nettype to none.
+  
+
+
+Error-[IND] Identifier not declared
+Starter RTL Code/GCN.sv, 249
+  Identifier 'CLASS_ACTIVE' has not been declared yet. If this error is not 
+  expected, please check if you have set `default_nettype to none.
+  
+
+
+Error-[IND] Identifier not declared
+Starter RTL Code/GCN.sv, 249
+  Identifier 'class_count' has not been declared yet. If this error is not 
+  expected, please check if you have set `default_nettype to none.
+  
+
+Parsing design file 'Testbench/GCN_TB.sv'
+3 errors
+CPU time: .604 seconds to compile
+ERROR: Compilation failed!
